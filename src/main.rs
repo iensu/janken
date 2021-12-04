d42e6e1c3f0df3964b70fdfdb9122c5a2ca2cf0e
Jens Östlund <jostlund@gmail.com>
2021-12-04 14:16:11 +0100

    Use RoundResult instead of bool

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,25 @@ fn main() {
 }
 
 #[derive(PartialEq, Debug)]
+enum RoundResult {
+    Win,
+    Loss,
+    Draw,
+}
+
+#[derive(PartialEq, Debug)]
 enum HandShape {
     Rock,
     Paper,
     Scissors,
 }
 
-fn is_winner(p1: HandShape, p2: HandShape) -> bool {
+fn is_winner(p1: HandShape, p2: HandShape) -> RoundResult {
     match (p1, p2) {
-        (HandShape::Rock, HandShape::Scissors) => true,
-        (HandShape::Scissors, HandShape::Paper) => true,
-        (HandShape::Paper, HandShape::Rock) => true,
-        _ => false,
+        (HandShape::Rock, HandShape::Scissors) => RoundResult::Win,
+        (HandShape::Scissors, HandShape::Paper) => RoundResult::Win,
+        (HandShape::Paper, HandShape::Rock) => RoundResult::Win,
+        _ => RoundResult::Loss,
     }
 }
 
@@ -24,16 +31,25 @@ mod tests {
 
     #[test]
     fn rock_wins_over_scissors() {
-        assert_eq!(true, is_winner(HandShape::Rock, HandShape::Scissors))
+        assert_eq!(
+            RoundResult::Win,
+            is_winner(HandShape::Rock, HandShape::Scissors)
+        )
     }
 
     #[test]
     fn scissors_wins_over_paper() {
-        assert_eq!(true, is_winner(HandShape::Scissors, HandShape::Paper))
+        assert_eq!(
+            RoundResult::Win,
+            is_winner(HandShape::Scissors, HandShape::Paper)
+        )
     }
 
     #[test]
     fn paper_wins_over_rock() {
-        assert_eq!(true, is_winner(HandShape::Paper, HandShape::Rock))
+        assert_eq!(
+            RoundResult::Win,
+            is_winner(HandShape::Paper, HandShape::Rock)
+        )
     }
 }
