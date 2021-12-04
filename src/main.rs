f1e2ff4baa04012bda5dd777d496fa826ea69532
Jens Östlund <jostlund@gmail.com>
2021-12-04 12:59:48 +0100

    Add test stump

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_meaning_of_life() {
+        let meaning_of_life = 42;
+
+        assert_eq!(42, meaning_of_life);
+    }
+}
