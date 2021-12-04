93b03e4fa4fb5b5e4ac3588fe601240c795bc71c
Jens Östlund <jostlund@gmail.com>
2021-12-04 13:43:24 +0100

    Implement remaining winning conditions

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ fn main() {
 fn is_winner(p1: &str, p2: &str) -> bool {
     if p1 == "rock" && p2 == "scissors" {
         return true;
+    } else if p1 == "scissors" && p2 == "paper" {
+        return true;
+    } else if p1 == "paper" && p2 == "rock" {
+        return true;
     }
 
     false
@@ -17,5 +21,15 @@ mod tests {
     #[test]
     fn rock_wins_over_scissors() {
         assert_eq!(true, is_winner("rock", "scissors"))
+    }
+
+    #[test]
+    fn scissors_wins_over_paper() {
+        assert_eq!(true, is_winner("scissors", "paper"))
+    }
+
+    #[test]
+    fn paper_wins_over_rock() {
+        assert_eq!(true, is_winner("paper", "rock"))
     }
 }
