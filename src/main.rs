23b1cab02fed5e647b858297d755d063e375cc89
Jens Östlund <jostlund@gmail.com>
2021-12-05 15:23:01 +0100

    Implemented to_handshape

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::io;
 
+use janken::HandShape;
+
+fn to_handshape(value: String) -> Result<HandShape, String> {
+    match value.trim().to_ascii_lowercase().as_str() {
+        "rock" => Ok(HandShape::Rock),
+        "paper" => Ok(HandShape::Paper),
+        "scissors" => Ok(HandShape::Scissors),
+        x => Err(format!("Can't turn '{}' into a hand shape", x)),
+    }
+}
+
 fn main() -> Result<(), io::Error> {
     let mut buffer = String::new();
     let stdin = io::stdin();
@@ -8,7 +19,10 @@ fn main() -> Result<(), io::Error> {
 
     stdin.read_line(&mut buffer)?;
 
-    println!("You wrote: {}", buffer);
+    match to_handshape(buffer) {
+        Ok(shape) => println!("Got {:?}", shape),
+        Err(err) => println!("Failed to get hand shape: {}", err),
+    }
 
     Ok(())
 }
